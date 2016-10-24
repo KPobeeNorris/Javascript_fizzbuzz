25735a47ea3d590c1302f293d954f3c6c3a7f7b6
Laurent Bouhnik <misterbl@hotmail.com>
2016-10-24 18:05:06 +0100

    fizzbuzz

--- Jasmine/spec/fizzBuzzSpec.js
+++ Jasmine/spec/fizzBuzzSpec.js
@@ -1,18 +1,27 @@
 
 describe("Fizzbuzz", function() {
 
-  var fizzbuzz;
-
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      fizzbuzz = new Fizzbuzz();
-      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+      expect(isDivisibleByThree(3)).toBe(true);
     });
 
+
     it('divisible by 5', function() {
-      fizzbuzz = new Fizzbuzz();
-      expect(fizzbuzz.isDvisibleByFive(5)).toBe(true);
+      expect(isDivisibleByFive(5)).toBe(true);
+    });
+  });
+
+  describe('it return fizz, buzz or fizzbuzz', function() {
+    it('returns fizz', function() {
+      expect(fizzBuzz(3)).toBe('fizz');
+    });
+    it('returns buzz', function() {
+      expect(fizzBuzz(5)).toBe('buzz');
+    });
+    it('returns fizzbuzz', function() {
+      expect(fizzBuzz(15)).toBe('fizzbuzz');
     });
   });
 });
--- Jasmine/src/fizzBuzz.js
+++ Jasmine/src/fizzBuzz.js
@@ -1,5 +1,21 @@
-var Fizzbuzz = function() {};
+// var Fizzbuzz = function() {};
 
-Fizzbuzz.prototype.isDivisibleByThree = function (number) {
+isDivisibleByThree = function (number) {
   return number % 3 == 0;
+};
+
+isDivisibleByFive = function (number) {
+  return number % 5 == 0;
+};
+
+fizzBuzz = function(number) {
+  if (isDivisibleByThree(number) && isDivisibleByFive(number)){
+  return "fizzbuzz";
+}
+  else if (isDivisibleByFive(number)) {
+  return "buzz";
+}
+  else if (isDivisibleByThree(number)) {
+  return "fizz";
+}
 };
